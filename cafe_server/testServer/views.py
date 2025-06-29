d93fe0ef5b53ef5723268ee18c43e41750a0ea1b
cuwuvaa <ivankolisa@gmail.com>
2025-06-29 19:51:00 +0300

    additional functions of the website

--- cafe_server/testServer/views.py
+++ cafe_server/testServer/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def hello(request):
     return HttpResponse(f"Hello!")
 
 def hello_user(request, name):
-    return render(request, "index.html", {"msg":name.capitalize()})
+    return render(request, "index.html", {"msg":name.capitalize(), "year":datetime.datetime.now().year})
+
