78ce6690e5126b9e54778593fffada5d2ec2b770
cuwuvaa <ivankolisa@gmail.com>
2025-06-30 17:51:45 +0300

    additional fixes and support od db

--- cafe_server/cafe/models.py
+++ cafe_server/cafe/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Dish(models.Model):
+    name = models.CharField(max_length=64)
+    rating = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return f"{self.name} with rating: {self.rating}"
--- cafe_server/cafe/views.py
+++ cafe_server/cafe/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from cafe.models import Dish
 
 def index(request):
-    return render(request,"main/index.html")
+    dishes = Dish.objects.all()
+    return render(request,"main/index.html", {"dishes":dishes})
