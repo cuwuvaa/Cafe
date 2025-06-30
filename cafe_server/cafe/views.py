e13ee572e8dbcd9cea75dee86d24ada32059cce8
cuwuvaa <ivankolisa@gmail.com>
2025-06-30 18:07:52 +0300

    integrating add func

--- cafe_server/cafe/views.py
+++ cafe_server/cafe/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django import forms
+from django.urls import reverse
 from cafe.models import Dish
+
+class SendDishForm(forms.Form):
+    name = forms.CharField(max_length=64)
+    rating = forms.IntegerField(max_value=10)
 
 def index(request):
     dishes = Dish.objects.all()
     return render(request,"main/index.html", {"dishes":dishes})
+
+def add_dish(request):
+    if request.method == "POST":
+        form = SendDishForm(request.POST)
+        if form.is_valid():
+            fname = form.cleaned_data["name"]
+            frating = form.cleaned_data["rating"]
+            s = Dish(name=fname, rating = frating).save()
+            return redirect(reverse('main'))
