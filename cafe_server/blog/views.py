020c56886d464c22cf835788c9c93aa9ce9ae362
cuwuvaa <ivankolisa@gmail.com>
2025-07-02 21:06:11 +0300

    show post func

--- cafe_server/blog/views.py
+++ cafe_server/blog/views.py
@@ -1,35 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from datetime import datetime
 from django import forms
 from .models import Post
 
 class CreatePostForm(forms.Form):
     text = forms.CharField(label="Text (250 chars)", max_length=250)
-    title_name = forms.CharField(label="Title of the post (64 chars)", max_length=64)
+    title = forms.CharField(label="Title of the post (64 chars)", max_length=64)
 
 def blogpage(request):
+    posts = Post.objects.all()
     return render(request, "blog/mainpage.html", 
-                  {"title_name":"Главная страница"})
+                  {
+                      "title_name":"Главная страница",
+                      "posts":posts
+                    })
 
 def add_post(request):
-    if request == "POST":
-        NewPost = Post() 
-
+    if request.method == "POST":
         form = CreatePostForm(request.POST)
-
-        title = form.cleaned_data["title"]
-        text = form.cleaned_data["text"]
-        NewPost.title = title
-        NewPost.text = text
-
-        NewPost.save()
-        
-        return render(request, "blog/createpost.html",
-                {
-                    "title_name":"Создать пост",
-                    "form":form
-                })
+        if form.is_valid():
+            formated_title = form.cleaned_data["title"]
+            formated_text = form.cleaned_data["text"]
+            NewPost = Post(title=formated_title, text=formated_text).save()
+            return HttpResponseRedirect(reverse('blog:main'))
     else:
         form = CreatePostForm()
         return render(request, "blog/createpost.html",
